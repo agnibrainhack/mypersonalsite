3c9a34e2658725aeda5ea29b01a9afe9ab2ae276
agnibrainhack <chakrabortynilagnik@gmail.com>
2018-08-15 22:31:30 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,8 @@ router.get('/resume', function(req, res, next) {
     res.sendFile(__dirname + '/static/nilagnik_resume_new.pdf')
 });
 
+router.get('/apk', function(req, res, next) {
+    res.sendFile(__dirname + '/static/WeCare.apk')
+});
+
 module.exports = router;
