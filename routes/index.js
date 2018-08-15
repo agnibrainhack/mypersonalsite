0e87501d2fdf84994db05c6b79d8f08f35b8036e
agnibrainhack <chakrabortynilagnik@gmail.com>
2018-08-15 23:41:53 +0530

    new v 1

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require("path");
+var http = require('http'),
+    fileSystem = require('fs');
 
 
 /* GET home page. */
@@ -9,11 +11,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/resume', function(req, res, next) {
+
     res.sendFile(__dirname + '/static/nilagnik_resume_new.pdf')
 });
 
 router.get('/apk', function(req, res, next) {
-    res.sendFile(__dirname + '/static/WeCare.apk')
+
+    var filePath = path.join(__dirname, '/static/WeCare.apk');
+    var stat = fileSystem.statSync(filePath);
+    //res.sendFile(__dirname + '/static/WeCare.apk')
+    res.writeHead(200, {
+        'Content-Type': 'application/vnd.android.package-archive',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(res);
+
 });
 
 module.exports = router;
